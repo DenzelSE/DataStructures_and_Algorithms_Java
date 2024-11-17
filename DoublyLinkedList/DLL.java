76bc32b7522a8d07ecec2f2fd5be44e0adabf987
DenzelSE <t.d.selokela@gmail.com>
2024-11-17 23:36:16 +0200

    DLL iteratinf and printing

--- DoublyLinkedList/DLL.java
+++ DoublyLinkedList/DLL.java
@@ -54,5 +54,12 @@ public class DLL {
             current = current.next;
         }
     }
-    
+    public void printList(){
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data + "");
+            current = current.next;
+        }
+        System.out.println();
+    }
 }
